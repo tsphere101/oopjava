2ae02254e95c47250b6e99573a882c02b2f7688d
Krissada Sarawit <topfeekrissada@outlook.com>
2021-02-17 11:53:02 +0700

    Refactorizing BMI class in lab0601 and Course class in lab0602

--- Lab06/lab0601/BMI.java
+++ Lab06/lab0601/BMI.java
@@ -1,41 +1,80 @@
 package lab0601;
 
 import java.lang.Math;
-// define 1 pound = 0.45359237 kg
 
-// define 1 inches = 0.0254 meters
+/**
+ * The class contains BMI calculation and data accumulation
+ * 
+ * @author TSphere
+ * @version 1.0
+ */
+
+
+/** Define 1 pound = 0.45359237 kg 
+  * Define 1 inches = 0.0254 meters 
+  */
 
 public class BMI {
-    String name = "None";
-    int age = 0;
-    double weight = 0; // stores in kg
-    double height = 0; // stores in meters
 
+    /**
+     * Name of the user
+     */
+    String name = "None";
+
+    /**
+     * Age of the user
+     */
+    int age = 0;
+
+    /**
+     * Weight of the user 
+     */
+    double weight = 0; /** Stores value in kilograms. */
+
+    /**
+     * Height of the user
+     */
+    double height = 0; /** Store value in meter. */
+
+    /**
+     * Construct user without specifying information
+     */
     public BMI() {
     }
 
+    /**
+     * Construct user with specifying weight and height
+     * @param weight weight in pounds
+     * @param height height in inches
+     */
     public BMI(double weight, double height) {
-        // check if there's negative value
-        if (weight < 0)
+        if (weight < 0) /** Check if weight or height are negative */
             weight = 0;
         if (height < 0)
             height = 0;
 
-        // converting
-        weight *= 0.45359237; // In pounds
-        weight = Math.round(weight * 100.0) / 100.0; // round
-        this.weight = weight; // stores in kilogram
-        height *= 0.0254; // In inches
-        height = Math.round(height * 100) / 100.0; // round
-        this.height = height; // stores in meter
+        weight *= 0.45359237; /* Converting pound to kilograms.*/
+        weight = Math.round(weight * 100.0) / 100.0; /** Rounding the decimal. */
+        this.weight = weight; /** Storing weight in Kilograms. */
+        height *= 0.0254; /** Converting inches to meter. */ 
+        height = Math.round(height * 100) / 100.0; /** Rounding the decimal. */
+        this.height = height; /** Storing height in meter. */
     }
 
+    /**
+     * Get the BMI result
+     * @return user's BMI value
+     */
     public double getBMI() {
-        double bmi = weight / ((height) * (height));
-        bmi = Math.round(bmi * 100.0) / 100.0;
+        double bmi = weight / ((height) * (height)); /** Calculating the BMI value */
+        bmi = Math.round(bmi * 100.0) / 100.0; /** Round the decimal. */
         return bmi;
     }
 
+    /**
+     * Get the BMI interpretation
+     * @return String that interpretate the BMI result
+     */
     public String getBmiInterpretation() {
         String output = "";
 
@@ -52,47 +91,78 @@ public class BMI {
         return output;
     }
 
+    /**
+     * Get user's height.
+     * @return user's height
+     */
     public double getHeight() {
         return height;
     }
 
+    /**
+     * Get user's weight.
+     * @return user's weight
+     */
     public double getWeight() {
         return weight;
     }
 
+    /**
+     * Get user's name.
+     * @return user's name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Get user's age.
+     * @return user's age
+     */
     public int getAge() {
         return age;
     }
 
+    /**
+     * Set user's height.
+     * @param height height in inches 
+     */
     public void setHeight(double height) {
-        if (height < 0) // if height is negative value
+        if (height < 0) /** In case of negative height value. */
             return;
-        height *= 0.0254; // In inches
-        height = Math.round(height * 100) / 100.0; // round
-        this.height = height;
+        height *= 0.0254; /** Converting inches to meter. */
+        height = Math.round(height * 100) / 100.0; /** Rounding the decimal. */
+        this.height = height; /** Stores value in meter. */
     }
 
+    /**
+     * Set user's weight.
+     * @param weight weight in lbs
+     */
     public void setWeight(double weight) {
-        if (weight < 0) // if weight is negative value
+        if (weight < 0) /** In case of negative weight value. */
             return;
-        weight *= 0.45359237; // In pounds
-        weight = Math.round(weight * 100.0) / 100.0; // round
-        this.weight = weight;
+        weight *= 0.45359237; /** Converting pounds to kilograms. */
+        weight = Math.round(weight * 100.0) / 100.0; /** Rounding the decimal. */
+        this.weight = weight; /** Stores data in kiligrams. */
     }
 
+    /**
+     * Set user's name.
+     * @param name the name of the user
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Set age of the user.
+     * @param age age of user
+     */
     public void setAge(int age) {
         if (age < 0)
             System.out.println("Your age is negative, you never born or what?");
             this.age = 0;
         this.age = age;
     }
-
 }
--- Lab06/lab0602/Course.java
+++ Lab06/lab0602/Course.java
@@ -1,51 +1,87 @@
 package lab0602;
 
+/**
+ * The class contains course's basic management, using in SchoolObjectExperiment project.
+ * 
+ * @author TSphere
+ * 
+ */
+
 public class Course {
 
-    // Fields
+    /**
+     * Dynamic array that store students in the course.
+     */
     private MyDynamicArray<Student> student_in_couse;
+
+    /**
+     * Course name.
+     */
     private String course_name;
 
-    // Constructor
+    /**
+     * Construct a course without course'name.
+     */
     public Course() {
         this("No Name", 0);
     }
 
-    public Course(String name) // Constructor for Course name defined
+    /**
+     * Construct a course with specify the name of the course.
+     * @param name name of the course
+     */
+    public Course(String name) 
     {
         this(name, 0);
     }
 
+    /**
+     * Construct a couse with sprcifying course's name and course's student amount.
+     * @param name name of the course
+     * @param student_amount amount of student think will be in the course
+     */
     public Course(String name, int student_amount) {
         this.course_name = name;
         student_in_couse = new MyDynamicArray<Student>(student_amount);
     }
-    // End Constructors
 
-    // Data field config
+    /**
+     * Set the name of the course.
+     * @param course_name the name of the course
+     */
     public void setCourseName(String course_name) {
         this.course_name = course_name;
     }
 
-    // Getters ======================
 
-    // Get Course Name
+    /**
+     * Get the name of the course.
+     * @return course's name
+     */
     public String getCourseName() {
         return course_name;
     }
 
-    // Get Student in couse amount
+    /**
+     * Get the amount of the student in course.
+     * @return amount of the student in course
+     */
     public int getStudentAmount() {
         return student_in_couse.getSize();
     }
 
-    // Method ================
-
-    // Remove Student by Index
+    /**
+     * Remove a student in course by index.
+     * @param index the index of the student stored in the array
+     */
     public void dropStudent(int index) {
         student_in_couse.drop(index);
     }
 
+    /**
+     * Remove a student in course by the student's object.
+     * @param student Student object
+     */
     public void dropStudent(Student student)
     {
         for(int i = 0 ; i < student_in_couse.getSize();i++)
@@ -57,7 +93,10 @@ public class Course {
         }
     }
 
-    // Remove Student by ID
+    /**
+     * Remove a student in course by specifying student's id.
+     * @param id ID of the student
+     */
     public void dropStudentById(int id) {
         for (int i = 0; i < student_in_couse.getSize(); i++) {
             if (student_in_couse.get(i).getId() == id) {
@@ -66,7 +105,10 @@ public class Course {
         }
     }
 
-    // Remove Student by Name
+    /**
+     * Remove a student in course by specifying student's name.
+     * @param name name of the student.
+     */
     public void dropStudentByName(String name)
     {
         boolean check_if_exist = false;
@@ -83,24 +125,35 @@ public class Course {
         }
     }
 
-    // Remove all student in the course
+    /**
+     * Remove all students in course.
+     */
     public void clear() {
         student_in_couse.clear();
     }
 
+    /**
+     * Add a student in course.
+     * @param student student's object
+     */
     public void addStudent(Student student) {
         student_in_couse.add(student);
     }
 
 
 
-    // Log
+    /**
+     * Log to console the course's informations.
+     */
     public void log()
     {
         System.out.println("Course Name::" + this.getCourseName());
         System.out.println("Student Amount::"+ this.getStudentAmount());
     }
 
+    /**
+     * Log to console the list of students in the course.
+     */
     public void logStudentList()
     {
         System.out.println("=== " + this.getCourseName() + " Student List ===");
